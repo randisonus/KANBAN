4a8a49978e23dd70597bee264ea4bceb5a7a6973
Zhenya <72355552+randisonus@users.noreply.github.com>
2022-04-13 16:45:56 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 const add = document.querySelector('.add');
-
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 function addCell() {
+    randomID = getRandomInt(0,10);
     const list = document.querySelector('.list');
     const cell1 = document.createElement('li');
     const cell2 = document.createElement('li');
     const cell3 = document.createElement('li');
-    cell1.classList.add('list__cell');
-    cell2.classList.add('list__cell');
-    cell3.classList.add('list__cell');
+    cell1.classList.add('list__cellnew');
+    cell2.classList.add('list__cellnew');
+    cell3.classList.add('list__cellnew');
     cell1.innerHTML = `
-    <div class="list__card" draggable="true">
+    <div class="list__card" id="${randomID}" draggable="true">
       <div class="list__card-header" contenteditable="true" >
         Введите название
       </div>
@@ -19,6 +22,7 @@ function addCell() {
       <div class="list__card-info">
         <textarea class="text" placeholder="Введите задачу"></textarea>
       </div>
+      <button class="del">Удалить карточку  
     </div>`;
 
   list.append(cell1);
@@ -87,3 +91,10 @@ const dragAndDrop = () => {
 
 dragAndDrop();
 
+const delet = document.querySelectorAll('.del');
+function deleteCell() {
+  const del = document.querySelectorAll('.list__cellnew');
+  del.remove();
+}
+
+delet.addEventListener('click', deleteCell);
